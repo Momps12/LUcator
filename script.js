31153dd81ed7336c743bd8cfa09c337d5ba2db7c
Momps12 <146367427+Momps12@users.noreply.github.com>
2023-11-12 22:40:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,6 @@
     // Esri.WorldImagery layer
     var esriLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri',
-        maxZoom: 22,
     });
 
     // Create a base layers object for the control
